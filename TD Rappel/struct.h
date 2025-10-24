ffcabd71b528d68746e7c1fc6117ed6bb12afb81
Mathieu <166537907+matlhieu@users.noreply.github.com>
2025-10-24 21:52:17 +0200

    Update struct.h

--- TD Rappel/struct.h
+++ TD Rappel/struct.h
@@ -1,63 +1,60 @@
 #ifndef STRUCT_H
 #define STRUCT_H
 
-typedef struct liste {
-    float nombre;
-    struct liste* suivant;
-} liste;
+#include <stddef.h>
 
-typedef struct grades {
-    char* nom;
-    liste* tab;
-    struct grades* suivant;
-    float moyenne;
-} grades;
+typedef struct {
+    double *values;
+    size_t size;
+    size_t capacity;
+} Grades;
 
-typedef struct course {
-    char* nom;
-    float coef;
-    struct course* suivant;
-} course;
+typedef struct {
+    char *name;
+    double coef;
+    Grades grades;
+    double average;
+} Course;
 
-typedef struct student {
-    char* prenom;
-    char* nom;
+typedef struct {
+    int id;
+    char *fname;
+    char *lname;
     int age;
-    int num;
-    grades* matieres;
-    float moyenne;
-    struct student* suivant;
-} student;
+    Course *courses;
+    size_t num_courses;
+    size_t courses_capacity;
+    double average;
+} Student;
 
-typedef struct prom {
-    student* tab_etu;
-    int nb_etu;
-    struct prom* suivant;
-} prom;
+typedef struct {
+    Student *students;
+    size_t num_students;
+    size_t students_capacity;
+} Prom;
 
-/* prototypes constructeurs / destructeurs */
-liste* ConstructeurListe(liste* a, float nb);
-float moyenneMatiere(liste* a);
-void ConstructeurMatiere(grades** a, float nb, char* mat);
-student* ajouterNotes(student* etu, char* mat, int id, float note);
-void ConstructeurCourse(course** matiere, char* a, float coef);
-student* ConstructeurStudent(student* etu, char* prenom, char* nom, int age, int num);
-student* moyenneEtu(student* stu, course* mat);
-prom* ConstructeurProm(prom* a, student* etu);
+/* --------------- Constructeurs / Destructeurs --------------- */
+Grades *grades_create(void);
+void grades_destroy(Grades *g);
+Course *course_create(const char *name, double coef);
+void course_destroy(Course *c);
+Student *student_create(int id, const char *fname, const char *lname, int age);
+void student_destroy(Student *s);
+Prom *prom_create(void);
+void prom_destroy(Prom *p);
 
-/* destructeurs */
-void destructeurListe(liste* a);
-void destructeurMat(grades* a);
-void destructeurStu(student* a);
-void destructeurProm(prom* a);
-void destructeurCourse(course* a);
+/* --------------- Fonctions utilitaires --------------- */
+int grades_add(Grades *g, double value);
+void course_update_average(Course *c);
+int course_add_grade(Course *c, double value);
+int student_add_course(Student *s, Course *c);
+void student_update_average(Student *s);
+int prom_add_student(Prom *p, Student *s);
+int safe_strdup(char **dest, const char *src);
 
-/* lecture / écriture fichier */
-student* lecture(course** matiere, const char* filename);
-int binaryWrite(prom* promotion, course* matiere);
-student* lectureBinaire(prom* p, course** matiere);
+/* --------------- Lecture et écriture fichiers --------------- */
+Prom *prom_from_file(const char *filename);
+int prom_save_binary(const char *filename, const Prom *p);
+Prom *prom_load_binary(const char *filename);
 
-/* affichages */
-void affichageStudent(student* a);
-void affichageCourse(course* a);
 #endif
