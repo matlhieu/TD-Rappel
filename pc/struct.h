2bb87b94b33da2d730426036456f8abbeb3fb6a3
Mathieu <166537907+matlhieu@users.noreply.github.com>
2025-10-26 18:36:25 +0100

    Add files via upload

---
+++ pc/struct.h
@@ -0,0 +1,46 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+#include <stdlib.h>
+
+//struct
+
+typedef struct{
+    double *values;
+    size_t size;
+    size_t capacity;
+}Grades;
+
+typedef struct{
+    char *name;
+    double coef;
+    Grades grades;
+    float passmark; 
+}Course;
+
+typedef struct{
+    int id;
+    char *fname;
+    char *lname;
+    int age;
+    Course* courses;
+    size_t num_course;
+    size_t capacity;
+    float average;
+}Student;
+
+typedef struct{
+    Student *students;
+    int numStudent;
+}Prom;
+
+//functions destroyyy
+
+
+void grades_destroy(Grades *g);
+void course_destroy(Course *c);
+void student_destroy(Student *s);
+void prom_destroy(Prom *p);
+
+
+
+#endif
