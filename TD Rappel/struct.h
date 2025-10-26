217436028ce7ee9c7b691fc1757b8a17c9db6e39
Mathieu <166537907+matlhieu@users.noreply.github.com>
2025-10-26 11:53:05 +0100

    Update struct.h

--- TD Rappel/struct.h
+++ TD Rappel/struct.h
@@ -4,13 +4,13 @@
 //struct
 
 typedef struct{
-    double* values;
+    double *values;
     size_t size;
     size_t capacity;
 }Grades;
 
 typedef struct{
-    char* name;
+    char *name;
     double coef;
     Grades grades;
     float passmark; 
@@ -18,8 +18,8 @@ typedef struct{
 
 typedef struct{
     int id;
-    char* fname;
-    char* lname;
+    char *fname;
+    char *lname;
     int age;
     Course* courses;
     size_t num_course;
@@ -28,28 +28,23 @@ typedef struct{
 }Student;
 
 typedef struct{
-    Student* students;
+    Student *students;
     int numStudent;
 }Prom;
 
 //functions
 
-Grades *grades_create();
+
 void grades_destroy(Grades *g);
 int grades_add(Grades *g, double value); 
 
-Course *course_create( char *name, double coef);
 void course_destroy(Course *c);
 int course_add_grade(Course *c, double value);
 
-
-Student *student_create(int id,  char *fname,  char *lname, int age);
 void student_destroy(Student *s);
 int student_add_course(Student *s, Course *c); 
 void student_update_average(Student *s);
 
-
-Prom *prom_create();
 void prom_destroy(Prom *p);
 int prom_add_student(Prom *p, Student *s); 
 
