5bf299271bb9cb656e4979c8da664d7492ace91a
Mathieu <166537907+matlhieu@users.noreply.github.com>
2025-10-26 17:03:44 +0100

    Update read_files.c

--- pc/read_files.c
+++ pc/read_files.c
@@ -10,7 +10,7 @@ static void trim(char *s) {
     memmove(s, p, strlen(p) + 1);
 }
 
-Prom *load_promotion_from_file(char *filename) {
+Prom *load_promotion_from_file(const char *filename) {
     FILE *f = fopen(filename, "r");
     if (!f) return NULL;
 
@@ -30,20 +30,14 @@ Prom *load_promotion_from_file(char *filename) {
             int id, age;
             char fname[50], lname[50];
             sscanf(line, "%d;%49[^;];%49[^;];%d", &id, fname, lname, &age);
+
             Student *s = student_create(id, fname, lname, age);
-            prom_add_student(promo, s);
-        }
-        else if (section == COURSES) {
-            char name[50]; double coef;
-            sscanf(line, "%49[^;];%lf", name, &coef);
-
-        }
-        else if (section == NOTES) {
-            int id; char cname[50]; double grade;
-            sscanf(line, "%d;%49[^;];%lf", &id, cname, &grade);
-
+            promo->students = realloc(promo->students, (promo->numStudent + 1) * sizeof(Student));
+            promo->students[promo->numStudent++] = *s;
+            free(s);
         }
     }
+
     fclose(f);
     return promo;
 }
