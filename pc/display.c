04f47e49e948ba1c092be401dbc275d0ad475692
Mathieu <166537907+matlhieu@users.noreply.github.com>
2025-10-26 18:22:51 +0100

    Update display.c

--- pc/display.c
+++ pc/display.c
@@ -1,18 +1,26 @@
+#include "struct.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-void display_promotion(Promo *p){
-    for (int i=0; i < p->numStudent; i++){
-        Student *s = &p->student[i];
-        printf("ID : %d | %s %s | %d ans\n", s->id, s-> fname, s->lname, s->age);
-        for (size_t j = 0; j < s->num_course; j++){
+void display_promotion(Prom *p) {
+    for (int i = 0; i < p->numStudent; i++) {
+        Student *s = p->students[i];
+        printf("ID : %d | %s %s | %d ans\n", s->id, s->fname, s->lname, s->age);
+
+        for (size_t j = 0; j < s->num_course; j++) {
             Course *c = &s->courses[j];
             printf("%s | coef %.2f : ", c->name, c->coef);
-            for (size_t k = 0; k < c->grades.size; k++){
-                printf("%.1f", c->grades.values[k]);
-            printf("\n");
+
+            double sum = 0;
+            for (size_t k = 0; k < c->grades.size; k++) {
+                printf("%.1f ", c->grades.values[k]);
+                sum += c->grades.values[k];
             }
-            printf("=> Average is : %.2f\n\n", s->average);
+
+            double passmark = (c->grades.size > 0) ? sum / c->grades.size : 0.0;
+            printf("| Average : %.2f\n", passmark);
         }
+
+        printf("=> Overall average : %.2f\n\n", s->average);
     }
 }
