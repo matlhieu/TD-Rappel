023c55c616b5c7255bfadea7154bcdf78c6b8209
Mathieu <166537907+matlhieu@users.noreply.github.com>
2025-10-26 16:36:11 +0100

    Add files via upload

---
+++ pc/display.h
@@ -0,0 +1,6 @@
+#ifdef DISPLAY_H
+#define DISPLAY_H
+
+void display_promotion(Promo *p);
+
+#endif
---
+++ pc/sort.h
@@ -0,0 +1,8 @@
+#ifdef SORT_H
+#define SORT_H
+#include <stlidb.h>
+
+int compare_students(const void *a, const void *b) 
+Student *get_top_students(Prom *p, int n)
+
+#endif
