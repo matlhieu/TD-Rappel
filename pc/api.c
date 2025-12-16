8d7d2084cd335cf4ab75d49d9c3b754a6c5fd919
Mathieu <166537907+matlhieu@users.noreply.github.com>
2025-12-16 14:56:26 +0100

    Add files via upload

---
+++ pc/api.c
@@ -0,0 +1,45 @@
+#include "struct.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h> 
+
+// PARTIE VERIF POLES
+
+// Fonction interne pour vérifier un masque
+static int check_validation(Student *s, uint64_t mask) {
+    // Si tous les bits du masque sont présents dans validated_courses
+    return (s->validated_courses & mask) == mask;
+}
+
+void API_display_results_per_field(Prom* pClass) {
+    if (!pClass) return;
+
+    printf("\n===================== Student Validation Status =====================\n");
+    printf("%-20s %-20s | %-10s | %-10s | %-8s | %-10s\n", "Last Name", "First Name", "Sciences", "Humanities", "Maths", "Full Year");
+    printf("-------------------------------------------------------------------------------------------\n");
+
+    for (int i = 0; i < pClass->numStudent; i++) {
+        Student *s = &pClass->students[i];
+
+        int val_sciences = check_validation(s, SCIENCES_MASK);
+        int val_maths = check_validation(s, MATHS_MASK);
+        int val_humanities = check_validation(s, HUMANITIES_MASK);
+
+        // Année validée si Sciences ET Humanités ET Maths sont validés
+        int val_year = check_validation(s, YEAR_MASK);
+
+        printf("%-20s %-20s | %-10s | %-10s | %-10s | %-10s |\n",
+               s->lname,
+               s->fname,
+               val_sciences ? "VALID" : "---",
+               val_humanities ? "VALID" : "---",
+               val_maths ? "VALID" : "---",
+               val_year ? "\x1b[32mPASSED\x1b[0m" : "\x1b[31mFAIL\x1b[0m");
+    }
+    printf("\n");
+}
+
+
+int save_promotion_binary(const char *filename, Prom *p);
+void load_promotion_binary(char* filename, Prom* p);
